511e075e99309efe86cc3aa5b2e314e5476dad9c
Fridolin <39649581+VonFriedricht@users.noreply.github.com>
2021-01-17 19:13:20 +0100

    initial commit

---
+++ solver.user.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         I swear i solved it wink
+// @version      0.1
+// @description  when you forgot the solution but want a puzzle marked as solved and get the points wink
+// @author       VonFriedricht
+// @match        https://twowi.de/
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+})();
